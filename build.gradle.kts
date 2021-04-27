20b49de369d534c099f92a6ae97ce52471176bbb
Sokolvyak Sergey <zhvkgj@yandex.ru>
2021-04-27 15:28:58 +0300

    initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,29 @@
+plugins {
+    id("org.jetbrains.intellij") version "0.7.3"
+    java
+}
+
+group = "org.example"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.6.0")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine")
+}
+
+// See https://github.com/JetBrains/gradle-intellij-plugin/
+intellij {
+    version = "2021.1 RC"
+}
+tasks.getByName<Test>("test") {
+    useJUnitPlatform()
+}
+tasks.getByName<org.jetbrains.intellij.tasks.PatchPluginXmlTask>("patchPluginXml") {
+    changeNotes("""
+      Add change notes here.<br>
+      <em>most HTML tags may be used</em>""")
+} 
---
+++ settings.gradle.kts
@@ -0,0 +1,2 @@
+rootProject.name = "lama-plugin"
+
