395ddd430c1787921b4d92beb7c5b80ea7ca93e8
sloboegen98 <sloboegen98@ya.ru>
2021-04-29 18:45:51 +0300

    feat: add icon for .lama file

---
+++ src/main/java/ru/mse/itmo/lama/icons/LamaIcons.java
@@ -0,0 +1,9 @@
+package ru.mse.itmo.lama.icons;
+
+import com.intellij.openapi.util.IconLoader;
+
+import javax.swing.*;
+
+public class LamaIcons {
+    public static final Icon FILE = IconLoader.getIcon("/icons/icon.svg");
+}
